cd2cd06e9ce85f6452459ab23bcb4dbc29de0356
Vishnu <vishnu@Vishnus-MacBook-Air.local>
2025-03-25 14:05:53 +0530

    uncomment outputs

--- project/docker-test/outputs.tf
+++ project/docker-test/outputs.tf
@@ -1,11 +1,11 @@
-# output "public_ip" {
-#   value = aws_instance.my_instance.public_ip  # get public_ip
-# }
+output "public_ip" {
+  value = aws_instance.example_instance.public_ip  # get public_ip
+}
 
-# output "ami_id" {
-#   value = aws_instance.my_instance.ami  # get ami_id
-# }
+output "ami_id" {
+  value = aws_instance.example_instance.ami  # get ami_id
+}
 
 # output "instance_tags" {
-#   value = aws_instance.my_instance.tags  # get instance_name
+#   value = aws_instance.example_instance.tags  # get instance_name
 # }
