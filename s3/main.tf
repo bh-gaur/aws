72c9f50e90cc7c77bf13bbd034b03d2a9becaf13
Vishnu <vishnu@Vishnus-MacBook-Air.local>
2025-04-13 13:47:36 +0530

    add .gitignore files

--- s3/main.tf
+++ s3/main.tf
@@ -1,8 +1,12 @@
-resource "aws_s3_bucket" "test-bucket" {
-  bucket = "testing"
+# resource "aws_s3_bucket" "tebucket" {
+#   bucket = "testing"
 
-  tags = {
-    Name = "My-bucket"
-    Environment = "Dev"
-  }
+#   tags = {
+#     Name = "My-bucket"
+#     Environment = "Dev"
+#   }
+# }
+resource "aws_s3_bucket" "example" {
+  bucket = "bhola-bucket"
+  
 }
